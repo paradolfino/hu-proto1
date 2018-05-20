1b852f5f9dfb7b17a90f3416c4dd45aa63f66e44
Viktharien <viktharien@zoho.com>
2018-05-20 23:09:30 +0000

    guest/host controller/model

---
+++ app/controllers/host_reviews_controller.rb
@@ -0,0 +1,45 @@
+class HostReviewsController < ApplicationController
+    
+    def create
+        
+        @reservation = Reservation.where(
+                id: host_review_params[:reservation_id],
+                room_id: host_review_params[:room_id],
+                user_id: host_review_params[:guest_id]
+            ).first
+            
+            if !@reservation.nil?
+                @has_reviewed = HostReview.where(
+                    reservation_id: @reservation.id,
+                    guest_id: host_review_params[:guest_id]
+                ).first
+                
+                if @has_reviewd.nil?
+                   @host_review = current_user.host_reviews.create(host_review_params)
+                   flash[:success] = "Review created..."
+                else
+                   flash[:alert] = "You already reviewed this!"
+                    
+                end
+            else
+                flash[:alert] = "Not found!"
+            end
+        
+        
+        redirect_back(fallback_location: request.referer)
+    end
+    
+    def destroy
+        @host_review = Review.find(params[:id])
+        @host_review.destroy
+        redirect_back(fallback_location: request.referer)
+    end
+    
+    
+    private
+    
+        def host_review_params
+            params.require(:host_review).permit(:comment, :star, :room_id, :reservation_id, :guest_id)
+        end
+        
+end
